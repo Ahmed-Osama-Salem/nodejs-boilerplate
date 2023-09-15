b97e72625556fc08aac70b244e62a2b09d13b1c2
Ahmed-Osama-Salem <ahmedosamaweb@gmail.com>
2023-09-15 16:00:08 +0300

    change settings

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,6 @@ const app = express();
 
 app.use(testRouter);
 
-app.listen(config.dev.port, () => {
-  console.log("Server is running on port" + config.dev.port);
+app.listen(config.dev.port || 8000, () => {
+  console.log("Server is running on port" + config.dev.port || 8000);
 });
